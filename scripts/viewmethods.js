cfbad59157b53fee370db7c33ad987cb893ad93d
Christopher Bruner <chrbruner9@gmail.com>
2016-09-23 09:53:34 -0700

    added markup to describe methods in viewmethods

--- scripts/viewmethods.js
+++ scripts/viewmethods.js
@@ -2,6 +2,7 @@
 
 var viewMethodsObject = {};
 
+//populate the select drop downs
 viewMethodsObject.populateSelect = function() {
   //TODO: this looks unsustainable so for your about me, consider not using an article as the div element for housing your about me blurb
   $('article[data-category]').each(function(){
@@ -20,6 +21,7 @@ viewMethodsObject.populateSelect = function() {
   });
 };
 
+//filter articles based on authors from select dropdown
 viewMethodsObject.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     var $thisName = $(this);
@@ -35,6 +37,7 @@ viewMethodsObject.handleAuthorFilter = function() {
   });
 };
 
+//filter articles based on categories from select dropdown
 viewMethodsObject.handleCategoryFilter = function() {
   $('#category-filter').on('change', function() {
     var $thisCategory = $(this);
@@ -50,6 +53,7 @@ viewMethodsObject.handleCategoryFilter = function() {
   });
 };
 
+//Put event listener on tabs so that it shows the section as indicated by the data tag and hides all other tabs
 viewMethodsObject.handleNavTabs = function() {
   $('#topheader').on('click', '.tab', function(event) {
     event.preventDefault();
