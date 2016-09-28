7ecd46cd0d09faf21454d65b81ff07c2f6516880
Christopher Bruner <chrbruner9@gmail.com>
2016-09-27 21:44:27 -0700

    refactored viewmethods.js and app.js to pass functions in fetchAll, changed it so building keys in myArticles used a more functional method, and used map reduce to get unique authors and refactored some loop methods to use map

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,59 @@
 'use strict';
 
-function myArticles(newObject) {
-  for (var key in newObject) {
-    this[key] = newObject[key];
+(function(module) {
+  //DONE: used map function to map objects keys to myArticles
+  function myArticles(newObject) {
+    var keys = Object.keys(newObject);
+    var that = this;
+    keys.map(function(element) {
+      that[element] = newObject[element];
+    });
   };
-}
 
-myArticles.all = [];
+  myArticles.all = [];
 
-myArticles.prototype.toHtml = function(scriptTemplateId) {
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? 'Published ' + this.daysAgo + ' days ago' : '(draft)';
-  var source = $(scriptTemplateId).html();
-  var template = Handlebars.compile(source);
-  var newArticle = template(this);
-  return newArticle;
-};
+  myArticles.prototype.toHtml = function(scriptTemplateId) {
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.publishStatus = this.publishedOn ? 'Published ' + this.daysAgo + ' days ago' : '(draft)';
+    var source = $(scriptTemplateId).html();
+    var template = Handlebars.compile(source);
+    var newArticle = template(this);
+    return newArticle;
+  };
 
-myArticles.loadAll = function(data) {
-  data.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  }).forEach(function(ele) {
-    myArticles.all.push(new myArticles(ele));
-  });
-};
+  //DONE: used reduce to produce the unique authors that have posted to this blog
+  myArticles.uniqueAuthors = function() {
+    return myArticles.all.reduce(function(prev, curr) {
+      if (prev.indexOf(curr.author) === -1) {
+        prev.push(curr.author);
+      }
+      return prev;
+    },[]).length;
+  };
 
-myArticles.fetchAll = function() {
-  var eTag = localStorage.getItem('ETag');
-  $.ajax({ url: 'data/fileobjects.json', success: function(data, textStatus, xhr) { localStorage.setItem('ETagCheck', xhr.getResponseHeader('ETag').slice(3, 20));}});
-  if (eTag === localStorage.ETagCheck) {
-    myArticles.loadAll(JSON.parse(localStorage.fileObjects));
-    viewMethodsObject.renderIndexPage();
-  } else {
-    $.ajax({ url: 'data/fileobjects.json', success: function(data, textStatus, xhr) { localStorage.setItem('fileObjects', xhr.responseText);
-      localStorage.setItem('ETag', xhr.getResponseHeader('ETag').slice(3, 20));
+  myArticles.loadAll = function(data) {
+    myArticles.all = data.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    }).map(function(object) {
+      //DONE: used map function to create new myArticles
+      return (new myArticles(object));
+    });
+  };
+
+  //DONE: modified fetchAll to accept a function
+  myArticles.fetchAll = function(nextFunction) {
+    var eTag = localStorage.getItem('ETag');
+    $.ajax({ type: 'HEAD', url: 'data/fileobjects.json', success: function(data, textStatus, xhr) { localStorage.setItem('ETagCheck', xhr.getResponseHeader('ETag').slice(3, 20));}});
+    if (eTag === localStorage.ETagCheck) {
       myArticles.loadAll(JSON.parse(localStorage.fileObjects));
-      viewMethodsObject.renderIndexPage();
-    }});
+      nextFunction();
+    } else {
+      $.ajax({ url: 'data/fileobjects.json', success: function(data, textStatus, xhr) { localStorage.setItem('fileObjects', xhr.responseText);
+        localStorage.setItem('ETag', xhr.getResponseHeader('ETag').slice(3, 20));
+        myArticles.loadAll(JSON.parse(localStorage.fileObjects));
+        nextFunction();
+      }});
+    };
   };
-};
+  module.myArticles = myArticles;
+})(window);
--- scripts/viewmethods.js
+++ scripts/viewmethods.js
@@ -1,70 +1,74 @@
 'use strict';
 
-var viewMethodsObject = {};
+(function(module) {
+  var viewMethodsObject = {};
 
-//filter articles based on authors from select dropdown
-viewMethodsObject.handleAuthorFilter = function() {
-  $('#author-filter').on('change', function() {
-    var $thisName = $(this);
-    var authorName = $thisName.val();
-    var $articles = $('article[data-author]');
-    if (authorName) {
-      $articles.hide();
-      $('article[data-author="' + authorName + '"]').show();
-    } else {
-      $articles.show();
-    };
-    $('#category-filter').val('');
-  });
-};
+  //filter articles based on authors from select dropdown
+  viewMethodsObject.handleAuthorFilter = function() {
+    $('#author-filter').on('change', function() {
+      var $thisName = $(this);
+      var authorName = $thisName.val();
+      var $articles = $('article[data-author]');
+      if (authorName) {
+        $articles.hide();
+        $('article[data-author="' + authorName + '"]').show();
+      } else {
+        $articles.show();
+      };
+      $('#category-filter').val('');
+    });
+  };
 
-//filter articles based on categories from select dropdown
-viewMethodsObject.handleCategoryFilter = function() {
-  $('#category-filter').on('change', function() {
-    var $thisCategory = $(this);
-    var categoryName = $thisCategory.val();
-    var $articles = $('article[data-category]');
-    if (categoryName) {
-      $articles.hide();
-      $('article[data-category="' + categoryName + '"]').show();
-    } else {
-      $articles.show();
-    }
-    $('#author-filter').val('');
-  });
-};
+  //filter articles based on categories from select dropdown
+  viewMethodsObject.handleCategoryFilter = function() {
+    $('#category-filter').on('change', function() {
+      var $thisCategory = $(this);
+      var categoryName = $thisCategory.val();
+      var $articles = $('article[data-category]');
+      if (categoryName) {
+        $articles.hide();
+        $('article[data-category="' + categoryName + '"]').show();
+      } else {
+        $articles.show();
+      }
+      $('#author-filter').val('');
+    });
+  };
 
-//Put event listener on tabs so that it shows the section as indicated by the data tag and hides all other tabs
-viewMethodsObject.handleNavTabs = function() {
-  $('#topheader').on('click', '.tab', function(event) {
-    event.preventDefault();
-    var content = $(this).data('content');
-    $('section[id="' + content + '"]').show();
-    $('section').not('#' + content).hide();
-  });
-  $('#topheader .tab:first').click();
-};
+  //Put event listener on tabs so that it shows the section as indicated by the data tag and hides all other tabs
+  viewMethodsObject.handleNavTabs = function() {
+    $('#topheader').on('click', '.tab', function(event) {
+      event.preventDefault();
+      var content = $(this).data('content');
+      $('section[id="' + content + '"]').show();
+      $('section').not('#' + content).hide();
+    });
+    $('#topheader .tab:first').click();
+  };
 
-viewMethodsObject.appendClearFix = function(domNode) {
-  domNode.append('<div class="clearfix"></div>');
-};
+  viewMethodsObject.appendClearFix = function(domNode) {
+    domNode.append('<div class="clearfix"></div>');
+  };
 
-viewMethodsObject.renderIndexPage = function() {
-  //Refactored renderIndexPage
-  myArticles.all.forEach(function(article) {
-    $('#projects').append(article.toHtml('#article-template'));
-    if ($('#author-filter option[value="' + article.author + '"]').length === 0) {
-      $('#author-filter').append(article.toHtml('#select-author-template'));
-    };
-    if ($('#category-filter option[value="' + article.category + '"]').length === 0) {
-      $('#category-filter').append(article.toHtml('#select-category-template'));
-    };
-  });
-  viewMethodsObject.handleAuthorFilter();
-  viewMethodsObject.handleCategoryFilter();
-  viewMethodsObject.handleNavTabs();
-  viewMethodsObject.appendClearFix($('#projects'));
-};
-
-//main
-myArticles.fetchAll();
+  viewMethodsObject.renderIndexPage = function() {
+    //Refactored renderIndexPage
+    myArticles.all.forEach(function(article) {
+      $('#projects').append(article.toHtml('#article-template'));
+      if ($('#author-filter option[value="' + article.author + '"]').length === 0) {
+        $('#author-filter').append(article.toHtml('#select-author-template'));
+      };
+      if ($('#category-filter option[value="' + article.category + '"]').length === 0) {
+        $('#category-filter').append(article.toHtml('#select-category-template'));
+      };
+    });
+    viewMethodsObject.handleAuthorFilter();
+    viewMethodsObject.handleCategoryFilter();
+    viewMethodsObject.handleNavTabs();
+    viewMethodsObject.appendClearFix($('#projects'));
+    $('#unique-authors b').text(myArticles.uniqueAuthors);
+  };
+  //main
+  module.viewMethodsObject = viewMethodsObject;
+  //DONE: call fetchAll on the renderIndexPage function
+  myArticles.fetchAll(viewMethodsObject.renderIndexPage);
+})(window);
