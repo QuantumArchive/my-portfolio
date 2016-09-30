0ca161b0e4d2b76a77f9b40031e217fcbf272671
Christopher Bruner <chrbruner9@gmail.com>
2016-09-29 18:46:43 -0700

    added new icon for admin page, added admin page along with basic functionality

--- scripts/model/app.js
+++ scripts/model/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 (function(module) {
-  //DONE: used map function to map objects keys to myArticles
   function myArticles(newObject) {
     var keys = Object.keys(newObject);
     var that = this;
@@ -18,11 +17,11 @@
     this.publishStatus = this.publishedOn ? 'Published ' + this.daysAgo + ' days ago' : '(draft)';
     var source = $(scriptTemplateId).html();
     var template = Handlebars.compile(source);
+    this.body = marked(this.body);
     var newArticle = template(this);
     return newArticle;
   };
 
-  //DONE: used reduce to produce the unique authors that have posted to this blog
   myArticles.uniqueAuthors = function() {
     return myArticles.all.reduce(function(prev, curr) {
       if (prev.indexOf(curr.author) === -1) {
@@ -36,24 +35,32 @@
     myArticles.all = data.sort(function(a,b) {
       return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
     }).map(function(object) {
-      //DONE: used map function to create new myArticles
       return (new myArticles(object));
     });
   };
 
-  //DONE: modified fetchAll to accept a function
   myArticles.fetchAll = function(nextFunction) {
     var eTag = localStorage.getItem('ETag');
-    $.ajax({ type: 'HEAD', url: 'data/fileobjects.json', success: function(data, textStatus, xhr) { localStorage.setItem('ETagCheck', xhr.getResponseHeader('ETag').slice(3, 20));}});
+    $.ajax({
+      type: 'HEAD',
+      url: 'data/fileobjects.json',
+      success: function(data, textStatus, xhr) {
+        localStorage.setItem('ETagCheck', xhr.getResponseHeader('ETag').slice(3, 20));
+      }
+    });
     if (eTag === localStorage.ETagCheck) {
       myArticles.loadAll(JSON.parse(localStorage.fileObjects));
       nextFunction();
     } else {
-      $.ajax({ url: 'data/fileobjects.json', success: function(data, textStatus, xhr) { localStorage.setItem('fileObjects', xhr.responseText);
-        localStorage.setItem('ETag', xhr.getResponseHeader('ETag').slice(3, 20));
-        myArticles.loadAll(JSON.parse(localStorage.fileObjects));
-        nextFunction();
-      }});
+      $.ajax({
+        url: 'data/fileobjects.json',
+        success: function(data, textStatus, xhr) {
+          localStorage.setItem('fileObjects', xhr.responseText);
+          localStorage.setItem('ETag', xhr.getResponseHeader('ETag').slice(3, 20));
+          myArticles.loadAll(JSON.parse(localStorage.fileObjects));
+          nextFunction();
+        }
+      });
     };
   };
   module.myArticles = myArticles;
---
+++ scripts/view/adminView.js
@@ -0,0 +1,35 @@
+'use strict';
+
+(function(module) {
+  var adminViewMethods = {};
+
+  adminViewMethods.adminPageInit = function() {
+    $('json-preview').on('focus', function() {
+      $(this).select();
+    });
+    $('#new-article').on('change', adminViewMethods.newArticleCreate);
+  };
+
+  adminViewMethods.newArticleCreate = function() {
+    $('#new-article-preview').empty();
+    var newArticle = new myArticles({
+      author : $('#new-author').val(),
+      title: $('#new-title').val(),
+      category: $('#new-category').val(),
+      link: $('#new-link').val(),
+      publishedOn: $('#new-publish').length ? new Date() : null,
+      image: $('#new-image').val(),
+      body: $('#new-body').val(),
+    });
+
+    $('#new-article-preview').append(newArticle.toHtml('#article-template'));
+    $('pre code').each(function(i, block) {
+      hljs.highlightBlock(block);
+    });
+
+    $('#json-preview').text(JSON.stringify(newArticle) + ',');
+  };
+  //main
+  module.adminViewMethods = adminViewMethods;
+  adminViewMethods.adminPageInit();
+})(window);
