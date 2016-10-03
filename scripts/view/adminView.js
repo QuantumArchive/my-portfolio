272cc30f88c810ab69f47ec38b0e98ed1be6247d
Christopher Bruner <chrbruner9@gmail.com>
2016-10-03 16:25:31 -0700

    modified admin page so there is a button that allows you to add multiple authors and included logic in adminView so that it can grab all the values and build a string based on it

--- scripts/view/adminView.js
+++ scripts/view/adminView.js
@@ -7,13 +7,33 @@
     $('json-preview').on('focus', function() {
       $(this).select();
     });
+    $('#addnewauthor').on('click', adminViewMethods.addNewAuthor);
     $('#new-article').on('change', adminViewMethods.newArticleCreate);
+  };
+
+  //event handler to deal with multiple authors for a given article
+  adminViewMethods.addNewAuthor = function() {
+    var $this = $(this);
+    var $inputNode = $('<input type="text" placeholder="Author Name" class="newauthors">');
+    $this.parents('#new-article').find('#author-label-append').append($inputNode);
+  };
+
+  //iterate through all authors on the author add page and return an array
+  adminViewMethods.getAuthors = function() {
+    var authors = [];
+    $('.newauthors').each(function(index, element) {
+      var elementVal = $(element).val();
+      if (0 < elementVal.length) {
+        authors.push($(element).val());
+      };
+    });
+    return authors;
   };
 
   adminViewMethods.newArticleCreate = function() {
     $('#new-article-preview').empty();
     var newArticle = new myArticles({
-      author : $('#new-author').val(),
+      author : adminViewMethods.getAuthors(),
       title: $('#new-title').val(),
       category: $('#new-category').val(),
       link: $('#new-link').val(),
@@ -21,7 +41,7 @@
       image: $('#new-image').val(),
       body: $('#new-body').val(),
     });
-
+    newArticle.buildAuthorString();
     $('#new-article-preview').append(newArticle.toHtml('#article-template'));
 
     $('pre code').each(function(i, block) {
